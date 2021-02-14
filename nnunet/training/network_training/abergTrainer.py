a861e2e15e2093ea9cee6a6e846a34d5f8c292c9
Andres Bergsneider <66797747+abergsneider@users.noreply.github.com>
2021-02-13 19:57:22 -0800

    modifying abergTrainer

--- nnunet/training/network_training/abergTrainer.py
+++ nnunet/training/network_training/abergTrainer.py
@@ -1,6 +1,16 @@
 #    [AB] Andres Bergsneider Modifications
+'''
+from typing import Tuple
+import numpy as np
+import torch
 
-from workspace.nnUNet.nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
+from nnunet.training.loss_functions.crossentropy import RobustCrossEntropyLoss
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
+from nnunet.training.network_training.nnUNetTrainerV2_DDP import nnUNetTrainerV2_DDP
+from nnunet.training.network_training.nnUNet_variants.architectural_variants.nnUNetTrainerV2_noDeepSupervision import \
+    nnUNetTrainerV2_noDeepSupervision
+from nnunet.utilities.to_torch import maybe_to_torch, to_cuda
+from torch.cuda.amp import autocast
 
 class abergTrainer_5epochs(nnUNetTrainerV2):
 
@@ -10,3 +20,5 @@ class abergTrainer_5epochs(nnUNetTrainerV2):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
         self.max_num_epochs = 5     # [AB] Limiting to 5
+
+        '''
