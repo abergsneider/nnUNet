6c8be9f304bcebc7d2c1b1e1b81b6d8a721c9c65
Andres Bergsneider <66797747+abergsneider@users.noreply.github.com>
2021-02-13 21:39:43 -0800

    modifying abergTrainer

--- nnunet/training/network_training/abergTrainer.py
+++ nnunet/training/network_training/abergTrainer.py
@@ -1,5 +1,5 @@
 #    [AB] Andres Bergsneider Modifications
-
+'''
 from collections import OrderedDict
 from typing import Tuple
 
@@ -14,14 +14,16 @@ from nnunet.network_architecture.neural_network import SegmentationNetwork
 from nnunet.training.data_augmentation.default_data_augmentation import default_2D_augmentation_params, \
     get_patch_size, default_3D_augmentation_params
 from nnunet.training.dataloading.dataset_loading import unpack_dataset
+'''
 from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2                    # [AB] Added "V2" at the end
+'''
 from nnunet.utilities.nd_softmax import softmax_helper
 from sklearn.model_selection import KFold
 from torch import nn
 from torch.cuda.amp import autocast
 from nnunet.training.learning_rate.poly_lr import poly_lr                                       # [AB] polyLR reducing training schedule
 from batchgenerators.utilities.file_and_folder_operations import *
-
+'''
 
 class abergTrainer(nnUNetTrainerV2):
 
@@ -30,4 +32,4 @@ class abergTrainer(nnUNetTrainerV2):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
-        self.max_num_epochs = 5     # [AB] Limiting to 5
+        self.max_num_epochs = 2     # [AB] Limiting to 5
