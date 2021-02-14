48d7f7acca5ad81fb1da9845ceaab1969496b95b
Andres Bergsneider <66797747+abergsneider@users.noreply.github.com>
2021-02-13 20:14:20 -0800

    modifying abergTrainer

--- nnunet/training/network_training/abergTrainer.py
+++ nnunet/training/network_training/abergTrainer.py
@@ -1,15 +1,25 @@
 #    [AB] Andres Bergsneider Modifications
+from collections import OrderedDict
 from typing import Tuple
+
 import numpy as np
 import torch
-
-from nnunet.training.loss_functions.crossentropy import RobustCrossEntropyLoss
-from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
-from nnunet.training.network_training.nnUNetTrainerV2_DDP import nnUNetTrainerV2_DDP
-from nnunet.training.network_training.nnUNet_variants.architectural_variants.nnUNetTrainerV2_noDeepSupervision import \
-    nnUNetTrainerV2_noDeepSupervision
+from nnunet.training.loss_functions.deep_supervision import MultipleOutputLoss2                 # [AB] Loss Function
 from nnunet.utilities.to_torch import maybe_to_torch, to_cuda
+from nnunet.training.data_augmentation.default_data_augmentation import get_moreDA_augmentation # [AB] Data Augmentation
+from nnunet.network_architecture.generic_UNet import Generic_UNet                               # [AB] Architecture CHECK <-------
+from nnunet.network_architecture.initialization import InitWeights_He
+from nnunet.network_architecture.neural_network import SegmentationNetwork
+from nnunet.training.data_augmentation.default_data_augmentation import default_2D_augmentation_params, \
+    get_patch_size, default_3D_augmentation_params
+from nnunet.training.dataloading.dataset_loading import unpack_dataset
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2                    # [AB] Added "V2" at the end
+from nnunet.utilities.nd_softmax import softmax_helper
+from sklearn.model_selection import KFold
+from torch import nn
 from torch.cuda.amp import autocast
+from nnunet.training.learning_rate.poly_lr import poly_lr                                       # [AB] polyLR reducing training schedule
+from batchgenerators.utilities.file_and_folder_operations import *
 
 class abergTrainer_5epochs(nnUNetTrainerV2):
 
